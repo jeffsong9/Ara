fe3f2c3ccc8fa7691519c5808411c14c61c97ed4
jeffsong9 <jeffsong9@gmail.com>
2018-12-31 22:57:26 -0500

    Update Rename_Files.R

--- code/Rename_Files.R
+++ code/Rename_Files.R
@@ -5,6 +5,9 @@ file_names<-list.files(pattern="*\\.txt")
 
 # Create file names. E.g. Files that start with 'CB' into 'RI_CB'
 gsub("^CB", "RI_CB", file_names)->new_file_names
+# e.g. ("^CH", "RI_CH", "CHCH_1.text")
+# e.g. ("CH", "RI_CH", "CHCH_1.text")
+
 
 # Rename files and loop them using the 'for' loop
 for(i in 1:length(file_names){
