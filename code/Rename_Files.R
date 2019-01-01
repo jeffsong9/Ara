cc28838c9b91aa2732c577e577b14b3ed853d985
jeffsong9 <jeffsong9@gmail.com>
2018-12-31 23:24:14 -0500

    Update Rename_Files.R

--- code/Rename_Files.R
+++ code/Rename_Files.R
@@ -4,16 +4,13 @@
 file_names<-list.files(pattern="*\\.txt")
 
 # Create file names. E.g. Files that start with 'CB' into 'RI_CB'
-gsub("^CH", "RICH", file_names)->new_file_names
-gsub("^GG", "RDGG", new_file_names)->new_file_names
-gsub("^GN", "RDGN", new_file_names)->new_file_names
-gsub("^GB", "RDGB", new_file_names)->new_file_names
-gsub("^CN", "RDCN", new_file_names)->new_file_names
-gsub("^CB", "RDCB", new_file_names)->new_file_names
+do.call("rbind", lapply(file_names, function(x){ifelse(grepl("^CH", x), paste0("RI", x), paste0("RD", x))}))->new_file_names
+# gsub("^CH", "RICH", file_names)->new_file_names
+# gsub("^GG", "RDGG", new_file_names)->new_file_names
 # e.g. gsub("^CH", "RI_CH", "CHCH_1.text")
 # e.g. gsub("CH", "RI_CH", "CHCH_1.text")
 
-# do.call("rbind", lapply(file_names, function(x){ifelse(grepl("^CH", x), paste0("RI", x), paste0("RD", x))}))->new_file_names
+
 
 
 # Rename files and loop them using the 'for' loop
